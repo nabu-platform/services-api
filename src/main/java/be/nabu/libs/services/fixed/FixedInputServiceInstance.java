cf48a1f9544acf7405d9810991ebcbda1b662699
alex <alex@nabu.be>
2016-10-13 17:07:00 +0200

    Fixed conversion of input, for java interface implementations the java stack will send the supertype instance, not the child instance

--- src/main/java/be/nabu/libs/services/fixed/FixedInputServiceInstance.java
+++ src/main/java/be/nabu/libs/services/fixed/FixedInputServiceInstance.java
@@ -4,6 +4,9 @@ import be.nabu.libs.services.api.ExecutionContext;
 import be.nabu.libs.services.api.Service;
 import be.nabu.libs.services.api.ServiceException;
 import be.nabu.libs.services.api.ServiceInstance;
+import be.nabu.libs.types.BaseTypeInstance;
+import be.nabu.libs.types.TypeConverterFactory;
+import be.nabu.libs.types.TypeUtils;
 import be.nabu.libs.types.api.ComplexContent;
 
 public class FixedInputServiceInstance implements ServiceInstance {
@@ -23,6 +26,14 @@ public class FixedInputServiceInstance implements ServiceInstance {
 
 	@Override
 	public ComplexContent execute(ExecutionContext executionContext, ComplexContent input) throws ServiceException {
+		// if the type of the input is not the same and not an extension, try to convert it
+		if (!(input.getType().equals(definition.getOriginal().getServiceInterface().getInputDefinition())) && TypeUtils.getUpcastPath(input.getType(), definition.getOriginal().getServiceInterface().getInputDefinition()).isEmpty()) {
+			Object converted = TypeConverterFactory.getInstance().getConverter().convert(input, new BaseTypeInstance(input.getType()), new BaseTypeInstance(definition.getOriginal().getServiceInterface().getInputDefinition()));
+			if (converted == null) {
+				throw new ServiceException("FIXED-1", "Can not convert input to requested format");
+			}
+			input = (ComplexContent) converted;
+		}
 		for (String key : definition.getInputKeys()) {
 			input.set(key, definition.getInput(key));
 		}
