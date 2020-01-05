4a0c19667a61c17a5488bf91c845cf5321268c92
Alexander Verbruggen <alex@nabu.be>
2020-01-05 20:00:05 +0100

    get service runner

--- src/main/java/be/nabu/libs/services/api/ServiceContext.java
+++ src/main/java/be/nabu/libs/services/api/ServiceContext.java
@@ -3,10 +3,14 @@ package be.nabu.libs.services.api;
 import be.nabu.libs.artifacts.api.Artifact;
 import be.nabu.libs.artifacts.api.ArtifactResolver;
 import be.nabu.libs.cache.api.CacheProvider;
+import be.nabu.libs.services.ServiceRunnerFactory;
 
 public interface ServiceContext {
 	public <T extends Artifact> ArtifactResolver<T> getResolver(Class<T> artifactType);
 	public CacheProvider getCacheProvider();
 	public ServiceRuntimeTrackerProvider getServiceTrackerProvider();
 	public ServiceAuthorizerProvider getServiceAuthorizerProvider();
+	public default ServiceRunner getServiceRunner() {
+		return ServiceRunnerFactory.getInstance().getServiceRunner();
+	}
 }
